fa30bfa59382be9c17d552f587f0c22171aee6ed
Maple_LeavesY <shishenxiao1@gmail.com>
2026-05-02 11:03:44 +0800

    题解更新
    56

--- Hot/Inc/56.h
+++ Hot/Inc/56.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 class Solution {
@@ -10,8 +11,23 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
     {
         vector<vector<int>> result;
+        sort(intervals.begin(), intervals.end());
+        vector<int> start = intervals[0], end = {};
         
-
-        
+        for(int i = 1; i < intervals.size(); i++)
+        {
+            end = intervals[i];
+            if(start[1] >= end[0])
+            {
+                start[1] = max(start[1], end[1]);
+            }
+            else
+            {
+                result.push_back(start);
+                start = end;
+            }
+        }
+        result.push_back(start);
+        return result;
     }
 };
