6f754be4ab981743bf9cc6de996e1f93f21f73bf
Maple_LeavesY <shishenxiao1@gmail.com>
2026-05-07 13:26:55 +0800

    题解更新

---
+++ Hot/Inc/160.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
+        
+        ListNode* temp;
+        ListNode* result;
+        int sum = 0;
+        temp = headA;
+        while(temp->val != NULL)
+        {
+            
+        }   
+    }
+};
