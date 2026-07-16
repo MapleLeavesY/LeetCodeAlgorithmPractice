cd2659c0aa9da25c8076c365245900b4a4f7b36f
Maple_LeavesY <shishenxiao1@gmail.com>
2026-07-16 11:00:46 +0800

    更新

--- Hot/Inc/131.h
+++ Hot/Inc/131.h
@@ -6,11 +6,18 @@ class Solution {
 public:
     std::vector<std::vector<std::string>> partition(std::string s) 
     {
-        
+        std::vector<std::vector<std::string>> result;
+        std::string path;
+
     }
 private:
-    void Prev()
+    void Prev
+    (
+        std::vector<std::vector<std::string>>& result,
+        std::string& path,
+        std::string& s
+    )
     {
-        
+
     }
 };
