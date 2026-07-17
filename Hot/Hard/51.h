f6365a32ac3c08406c0a9c8e3757edd55215c969
Maple_LeavesY <shishenxiao1@gmail.com>
2026-07-17 11:36:29 +0800

    更新

---
+++ Hot/Hard/51.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#pragma region n皇后 --回溯Hard
+
+class Solution {
+public:
+    std::vector<std::vector<std::string>> solveNQueens(int n) 
+    {
+        
+    }
+private:
+    void PreV
+    (
+        
+    )
+    {
+
+    }
+};
+
+#pragma endregion
--- Hot/Inc/131.h
+++ Hot/Inc/131.h
@@ -7,17 +7,54 @@ public:
     std::vector<std::vector<std::string>> partition(std::string s) 
     {
         std::vector<std::vector<std::string>> result;
-        std::string path;
+        std::vector<std::string> path;
 
+        Prev(result, path, s, 0);
+        return result;
     }
 private:
     void Prev
     (
         std::vector<std::vector<std::string>>& result,
-        std::string& path,
-        std::string& s
+        std::vector<std::string>& path,
+        std::string& s,
+        int select
     )
     {
+        if(select >= s.size())
+        {
+            result.push_back(path);
+            return;
+        }
 
+        for(int i = select; i < s.size(); i++)
+        {
+
+            if(!GetValueisChild(s, select, i)) continue;
+            
+            std::string temp;
+            temp = s.substr(select, i - select + 1);
+
+            path.push_back(temp);
+
+            Prev(result, path, s, i + 1);
+
+            path.pop_back();
+        }
+    }
+    bool GetValueisChild(std::string path, int start, int end)
+    {
+        if(path.size() == 0) return false;
+        int left = start;
+        int right = end;
+
+        while(left < right)
+        {
+            if(path[left] != path[right]) return false;
+
+            left++;
+            right--;
+        }
+        return true;
     }
 };
