79e426a01a4437ebb3fecc2d0a954c8fa061d4bb
Maple_LeavesY <shishenxiao1@gmail.com>
2026-08-26 12:57:58 +0800

    Update 152.h

--- Hot/Normal/152.h
+++ Hot/Normal/152.h
@@ -22,3 +22,37 @@ public:
         return result;
     }
 };
+
+
+
+
+
+
+#pragma region 最大子数组
+
+class Solution {
+public:
+    int maxProduct(std::vector<int>& nums) 
+    {
+        int currentMax { nums[0] };
+        int currentMin { nums[0] };
+        int size = nums.size();
+        int result{ nums[0] };
+        for(int i = 1; i < size; i++)
+        {
+            int lastMax = currentMax;
+            int lastMin = currentMin;
+
+            currentMax = std::max(nums[i] * lastMax, nums[i]);
+            currentMax = std::max(nums[i] * lastMin, currentMax);
+            
+            currentMin = std::min(nums[i] * lastMin, nums[i]);
+            currentMin = std::min(nums[i] * lastMax, currentMin);
+
+            result = std::max(result, currentMax);
+        }
+        return result;
+    }
+};
+
+#pragma endregion
