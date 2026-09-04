b774769fd0a3c9e6efa573b49eafab732691cf12
Maple_LeavesY <shishenxiao1@gmail.com>
2026-09-04 12:13:36 +0800

    Update 62.h

--- Hot/Normal/62.h
+++ Hot/Normal/62.h
@@ -1,11 +1,13 @@
+#include <vector>
 
+#pragma region 多维动态规划
 enum class Dirction
 {
     Right,
     Down,
 };
 
-
+// 自己思考的
 class Solution {
 public:
     int uniquePaths(int m, int n) 
@@ -46,3 +48,33 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) 
+    {
+        std::vector<std::vector<int>> dp
+        (
+            m,
+            std::vector<int>(n, 0)
+        );
+        dp[0][0] = 1;
+        for(int y = 0; y < m; y++)
+        {
+            for(int x = 0; x < n; x++)
+            {
+                if(y - 1 >= 0)
+                {
+                    dp[y][x] += dp[y - 1][x];
+                }
+                if(x - 1 >= 0)
+                {
+                    dp[y][x] += dp[y][x - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+#pragma endregion
