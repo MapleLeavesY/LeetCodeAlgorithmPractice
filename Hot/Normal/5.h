a0cec81fecfde929b398a5d46a7ff10a5d19ac28
Maple_LeavesY <shishenxiao1@gmail.com>
2026-09-06 22:38:08 +0800

    Update 5.h

--- Hot/Normal/5.h
+++ Hot/Normal/5.h
@@ -36,3 +36,65 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) 
+    {
+        int start = 0;
+        int end = 0;
+        int size = s.size();
+        for(int i = 0; i < size; i++)
+        {
+            Catlage(s, i, i, start, end);
+
+            Catlage(s, i, i + 1, start, end);
+        }
+        return s.substr(start, end - start + 1);
+    }
+private:
+    void Catlage
+    (
+        const std::string& str,
+        int leftBegin,
+        int rightBegin,
+        int& start,
+        int& end
+    )
+    {
+        if(leftBegin == rightBegin)
+        {//中心点相同，为奇数对称
+            int left = leftBegin;
+            int right = leftBegin;
+
+            while(left >= 0 && right < str.size())
+            {
+                if(str[left] != str[right]) break;
+                if(end - start < right - left)
+                {
+                    start = left;
+                    end = right;
+                }
+                left--;
+                right++;
+            }
+            return;
+        }
+        //中心点左右对称，为偶数对称
+        int left = leftBegin;
+        int right = rightBegin;
+
+        while(left >= 0 && right < str.size())
+        {
+            if(str[left] != str[right]) break;
+            if(end - start < right - left)
+            {
+                start = left;
+                end = right;
+            }
+            left--;
+            right++;
+        }
+    }
+};
